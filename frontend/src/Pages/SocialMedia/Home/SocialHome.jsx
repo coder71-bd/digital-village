0ec7c76e2cd257728c2594346362969b81c6acf9
Jubayer__Hossain <jubayer.dev71@gmail.com>
2022-12-25 23:33:51 +0600

    socket removed

--- frontend/src/Pages/SocialMedia/Home/SocialHome.jsx
+++ frontend/src/Pages/SocialMedia/Home/SocialHome.jsx
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { CgMenuGridO } from 'react-icons/cg';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { io } from 'socket.io-client';
-import { BASE_URI } from '../../../api/axios';
-import useMediaQuery from '../../../hooks/useMediaQuery';
-import { allSocialUser } from '../../../redux/slices/socialSlice/socialSlice';
-import Feeds from './Feeds/Feeds';
-import LeftSideMenu from './LeftSideMenu/LeftSideMenu';
-import PublishFeed from './PublishFeed/PublishFeed';
-import RightSide from './RightSide/RightSide';
+import React, { useEffect, useState } from "react";
+import { CgMenuGridO } from "react-icons/cg";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+// import { io } from 'socket.io-client';
+import useMediaQuery from "../../../hooks/useMediaQuery";
+import { allSocialUser } from "../../../redux/slices/socialSlice/socialSlice";
+import Feeds from "./Feeds/Feeds";
+import LeftSideMenu from "./LeftSideMenu/LeftSideMenu";
+import PublishFeed from "./PublishFeed/PublishFeed";
+import RightSide from "./RightSide/RightSide";
 
 const SocialHome = () => {
   const user = useSelector((state) => state.user.user);
-  const isDesktop = useMediaQuery('(min-width: 900px)');
+  const isDesktop = useMediaQuery("(min-width: 900px)");
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(allSocialUser(user?.email));
   }, [user]);
   const [menuopen, setMenuOpen] = useState(false);
 
-  var socket;
-  useEffect(() => {
-    socket = io(BASE_URI);
-  }, []);
+  // var socket;
+  // useEffect(() => {
+  //   socket = io(BASE_URI);
+  // }, []);
   return (
     <div
       className="mt-[80px] mb-36 md:mb-24"
       style={{
-        minHeight: isDesktop ? 'calc(100vh - 88px)' : 'fit-content',
+        minHeight: isDesktop ? "calc(100vh - 88px)" : "fit-content",
       }}
     >
       <div className="relative grid grid-cols-7 md:gap-6">
@@ -41,7 +40,7 @@ const SocialHome = () => {
           <CgMenuGridO onClick={() => setMenuOpen(!menuopen)} size={27} />
           <div
             className={`${
-              menuopen ? 'absolute' : 'hidden'
+              menuopen ? "absolute" : "hidden"
             } bg-slate-300 px-2 py-3 space-y-2`}
           >
             <Link className="block" to="/userdashboard/profile">
